7eb19a5fa6fc107a2b131ffb8c64361202d36e5e
Viktor Pasynok <you@example.com>
2019-11-10 01:11:23 +0300

    fix: lefthook config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 const multiply = (module: number, factor: number) => `${module * factor}px`;
 
 export const createSpacing = (module: number = 8) => (first: number, ...rest: ReadonlyArray<number>): string =>
-  rest.reduce((acc, item, index) => `${acc} ${multiply(module, item)}`, multiply(module, first));
+  rest.reduce((acc, item) => `${acc} ${multiply(module, item)}`, multiply(module, first));
