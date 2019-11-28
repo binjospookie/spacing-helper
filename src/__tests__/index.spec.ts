4215eac005384d5ea12bdc3eee5d3b275211c6ae
Viktor Pasynok <binjospookie@gmail.com>
2019-11-28 22:26:17 +0300

    test: change jest to ava

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,36 +1,36 @@
+import test from 'ava';
+
 import { createSpacing } from '../';
 
 const spacingDefault = createSpacing();
 const spacingEight = createSpacing(8);
 const spacingFour = createSpacing(4);
 
-describe('Test cases', () => {
-  it('Single argument', () => {
-    expect(spacingEight(2)).toBe('16px');
-    expect(spacingEight(0.5)).toBe('4px');
+test('Single argument', ({ is }) => {
+  is(spacingEight(2), '16px');
+  is(spacingEight(0.5), '4px');
 
-    expect(spacingDefault(2)).toBe('16px');
+  is(spacingDefault(2), '16px');
 
-    expect(spacingFour(2)).toBe('8px');
-    expect(spacingFour(0.5)).toBe('2px');
-  });
+  is(spacingFour(2), '8px');
+  is(spacingFour(0.5), '2px');
+});
 
-  it('Some arguments', () => {
-    expect(spacingEight(1, 2)).toBe('8px 16px');
-    expect(spacingEight(1, 2, 3)).toBe('8px 16px 24px');
-    expect(spacingEight(1, 2, 3, 4)).toBe('8px 16px 24px 32px');
+test('Some arguments', ({ is }) => {
+  is(spacingEight(1, 2), '8px 16px');
+  is(spacingEight(1, 2, 3), '8px 16px 24px');
+  is(spacingEight(1, 2, 3, 4), '8px 16px 24px 32px');
 
-    expect(spacingDefault(1, 2)).toBe('8px 16px');
-    expect(spacingDefault(1, 2, 3)).toBe('8px 16px 24px');
-    expect(spacingDefault(1, 2, 3, 4)).toBe('8px 16px 24px 32px');
+  is(spacingDefault(1, 2), '8px 16px');
+  is(spacingDefault(1, 2, 3), '8px 16px 24px');
+  is(spacingDefault(1, 2, 3, 4), '8px 16px 24px 32px');
 
-    expect(spacingFour(1, 2)).toBe('4px 8px');
-    expect(spacingFour(1, 2, 3)).toBe('4px 8px 12px');
-    expect(spacingFour(1, 2, 3, 4)).toBe('4px 8px 12px 16px');
-  });
+  is(spacingFour(1, 2), '4px 8px');
+  is(spacingFour(1, 2, 3), '4px 8px 12px');
+  is(spacingFour(1, 2, 3, 4), '4px 8px 12px 16px');
+});
 
-  it('Incorrect argument', () => {
-    // @ts-ignore
-    expect(spacingEight()).toBe('NaNpx');
-  });
+test('Incorrect argument', ({ is }) => {
+  // @ts-ignore
+  is(spacingEight(), 'NaNpx');
 });
