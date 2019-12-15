fee4890b79a4628f7433d301bc6c800efa19a726
Viktor Pasynok <binjospookie@gmail.com>
2019-12-15 22:47:38 +0300

    feat: improve type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 const multiply = (module: number, factor: number) => `${module * factor}px`;
 
-export const createSpacing = (module: number = 8) => (first: number, ...rest: ReadonlyArray<number>): string =>
+export const createSpacing = (module: number = 8) => (first: number, ...rest: readonly number[]): string =>
   rest.reduce((acc, item) => `${acc} ${multiply(module, item)}`, multiply(module, first));
