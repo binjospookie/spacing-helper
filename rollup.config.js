8e5bce0b4f606e296c94c0d435c14f91c8f60bca
Viktor Pasynok <binjospookie@gmail.com>
2020-01-01 21:31:19 +0300

    build: change plugin to build ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import { terser } from 'rollup-plugin-terser';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
 
 import pkg from './package.json';
 
