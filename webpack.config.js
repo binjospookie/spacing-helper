08e9f6093ffc44fbbbffdb616b0dd0e4cca7a0d6
Viktor Pasynok <binjospookie@gmail.com>
2020-03-31 22:24:11 +0300

    build: try webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.ts' ],
+  },
+  output: {
+    filename: 'index.es.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+};
