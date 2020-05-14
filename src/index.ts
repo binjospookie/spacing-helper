73d55e9be081056a361db947141a2df0ab095be4
Viktor Pasynok <binjospookie@gmail.com>
2020-05-15 00:19:18 +0300

    perf: reduce bundle to 91b

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,12 @@ interface CreateSpacing {
   readonly units?: string;
 }
 
-const makeTransform = ({ units, precision, factor }: Omit<Required<CreateSpacing>, 'divisor'>) => (spacing: number) =>
-  `${~~(spacing * factor) / precision}${units}`;
-
-const spacingRaw = (transform: ReturnType<typeof makeTransform>) => (first: number = 1, ...data: readonly number[]) =>
-  data.reduce((acc, item) => `${acc} ${transform(item)}`, transform(first));
-
 export const createSpacing = ({ factor = 8, divisor = 1, precision = 2, units = 'px' }: CreateSpacing) => {
   const parsedPrecision = 10 ** precision;
   const factorParsed = (factor / divisor) * parsedPrecision;
-  const transform = makeTransform({ factor: factorParsed, precision: parsedPrecision, units });
 
-  return spacingRaw(transform);
+  const transform = (spacing: number) => `${~~(spacing * factorParsed) / parsedPrecision}${units}`;
+
+  return (first: number = 1, ...data: readonly number[]) =>
+    data.reduce((acc, item) => `${acc} ${transform(item)}`, transform(first));
 };
