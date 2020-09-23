bbb95bf59ca01dbf864d6548b381414812729c9d
Viktor Pasynok <binjospookie@gmail.com>
2020-09-23 22:48:51 +0300

    build: 3 bytes reduce lol

--- src/index.js
+++ src/index.js
@@ -18,13 +18,13 @@ export const createSpacing = ({ factor = 8, divisor = 1, precision = 2, units = 
   return (first = 1, second, third, fourth) => {
     let res = transform(first);
 
-    if (typeof second !== 'number') {
+    if (second / 1 !== second) {
       return res;
     }
 
     res = `${res} ${transform(second)}`;
 
-    if (typeof third !== 'number') {
+    if (third / 1 !== third) {
       return res;
     }
 
